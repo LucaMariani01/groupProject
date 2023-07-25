883515218e7b0c5ae7683931d80b693a7b8b81f6
Diezz01 <diegopennesi01@gmail.com>
2023-07-25 11:38:17 +0200

    Fix errori. creazione nuovo file funzionante.

---
+++ FileManagerTSV.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+
+public class FileManagerTSV {
+    /** metodo per leggere il file tsv
+     *
+     * @param fileTSV nome del file da leggere
+     * @param start inizio dell'intervallo di interesse
+     * @param end fine dell'intervallo di interesse
+     * @param pdb
+     * @return regione di interesse
+     */
+    public ArrayList<String[]> tsvr(File fileTSV, int start, int end, String pdb) {
+        /**
+         * Arraylist delle righe lette dal file tsv
+         */
+        ArrayList<String[]> Data = new ArrayList<>();
+        int cont =0 ;
+        // leggo e registro le righe del file tsv
+        try (BufferedReader TSVReader = new BufferedReader(new FileReader(fileTSV))) {
+            String line = null;
+            while ((line = TSVReader.readLine()) != null) {
+                if(cont== 0) cont=1;
+                else {
+                    String[] lineItems = line.split("\t");
+
+                    if (Integer.parseInt(lineItems[4]) >= start && Integer.parseInt(lineItems[5]) <= end && pdb.compareTo(lineItems[2]) == 0)
+                        Data.add(lineItems);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        }
+        return Data;
+    }
+
+    /**
+     * Metodo che crea il nuovo file TSV
+     * @param fileData righe da scrivere nel nuovo file
+     * @throws IOException
+     */
+    public void createFileTSV(ArrayList<String[]> fileData)throws IOException {
+        //creo il nuovo file tsv contente l'intervallo di interesse
+        try (PrintWriter writer = new PrintWriter(
+                Files.newBufferedWriter(Paths.get("RepeatsDB-table-trimmed.tsv")))) {
+            writer.println(
+                    "Reviewed\t"+ "Classification\t"+ "RepeatsDB ID\t"+ "Type\t"+ "start\t"+ "end"
+            );
+            //scrivo nel file le righe intressate
+            for (String[] row : fileData) {
+                writer.printf("%1$20s\t%2$3s\t%3$3s\t%4$3s\t%5$s\t%6$s",
+                        row[0], row[1], row[2], row[3], row[4], row[5]);
+                writer.println();
+            }
+        }
+    }
+}
--- provaMain.java
+++ provaMain.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -9,11 +7,10 @@ public class provaMain {
     public static void main(String[] args)throws Exception {
         int start=35,end=247;
         String pdb = "6gaoC";
-        ReadFileTSV fileReader = new ReadFileTSV();
+        FileManagerTSV fileReader = new FileManagerTSV();
         ArrayList<String[]> array = fileReader.tsvr(new File("RepeatsDB-table.tsv"),start,end,pdb);
 
         fileReader.createFileTSV(array);
-        array.forEach(a -> System.out.println(Arrays.toString(a)));
 
     }
 

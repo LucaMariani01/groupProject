e3885acc18e682b07fe9bc9a7ed466ac073a7e4e
Diezz01 <diegopennesi01@gmail.com>
2023-07-25 11:01:15 +0200

    Metodi per leggere la tabella PDB e generare un file contenente legami in un certo intervallo dato

--- provaMain.java
+++ provaMain.java
@@ -6,32 +6,16 @@ import java.util.Arrays;
 
 public class provaMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
         int start=35,end=247;
         String pdb = "6gaoC";
-        ArrayList<String[]> array= tsvr(new File("RepeatsDB-table.tsv"),start,end,pdb);
+        ReadFileTSV fileReader = new ReadFileTSV();
+        ArrayList<String[]> array = fileReader.tsvr(new File("RepeatsDB-table.tsv"),start,end,pdb);
 
+        fileReader.createFileTSV(array);
         array.forEach(a -> System.out.println(Arrays.toString(a)));
 
     }
 
-    public static ArrayList<String[]> tsvr(File test2,int start,int end,String pdb) {
-        ArrayList<String[]> Data = new ArrayList<>(); //initializing a new ArrayList out of String[]'s
-        int cont =0 ;
-        try (BufferedReader TSVReader = new BufferedReader(new FileReader(test2))) {
-            String line = null;
-            while ((line = TSVReader.readLine()) != null) {
-                if(cont== 0) cont=1;
-                else {
-                    String[] lineItems = line.split("\t"); //splitting the line and adding its items in String[]
 
-                    if (Integer.parseInt(lineItems[4]) >= start && Integer.parseInt(lineItems[5]) <= end && pdb.compareTo(lineItems[2]) == 0)
-                        Data.add(lineItems); //adding the splitted line array to the ArrayList
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("Something went wrong");
-        }
-        return Data;
-    }
 }

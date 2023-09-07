95f34331bcabd819f75700eacf70185daf0709cb
montecoMC <46907183+montecoMC@users.noreply.github.com>
2023-09-07 16:48:04 +0200

    commit to test FIlippo

--- FileManagerTSV.java
+++ FileManagerTSV.java
@@ -37,6 +37,36 @@ public class FileManagerTSV {
         return Data;
     }
 
+    public String[] tsvr(File fileTSV, String pdb){
+        int minStart=-1, maxEnd=-1;
+        ArrayList<String[]> Data = new ArrayList<>();
+
+        int cont =0 ;
+        try (BufferedReader TSVReader = new BufferedReader(new FileReader(fileTSV))) {
+            String line = null;
+            while ((line = TSVReader.readLine()) != null) {
+                if(cont== 0) cont=1;
+                else {
+                    String[] lineItems = line.split("\t");
+                    if(pdb.compareTo(lineItems[2]) == 0) {
+                        if (minStart == -1 && maxEnd == -1 ) {
+                            minStart = Integer.parseInt(lineItems[4]);
+                            maxEnd = Integer.parseInt(lineItems[5]);
+                        }else {
+                            if (Integer.parseInt(lineItems[4]) < minStart)   minStart = Integer.parseInt(lineItems[4]);
+                            if (Integer.parseInt(lineItems[5]) > maxEnd )  maxEnd = Integer.parseInt(lineItems[5]);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Something went wrong");
+        }
+        String[] ritorno= new String[10];
+        ritorno[0] = ""+pdb+"\t"+minStart+"\t"+maxEnd;
+        return (ritorno);
+    }
+
     /**
      * Metodo che crea il nuovo file TSV
      * @param fileData righe da scrivere nel nuovo file

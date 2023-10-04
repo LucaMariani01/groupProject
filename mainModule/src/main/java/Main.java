96117c0be71ab4d8c885843c4b0cf22ab43c8aa1
Diezz01 <diegopennesi01@gmail.com>
2023-10-04 17:23:43 +0200

    Path da linea comando

--- mainModule/src/main/java/Main.java
+++ mainModule/src/main/java/Main.java
@@ -1,24 +1,26 @@
 package main.java;
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
+        String[] arg;
         String fileJson = "RepeatsDB-table.json";
         FileManagerTSV fileReader = new FileManagerTSV();
         ArrayList<String> pdbList = fileReader.getPDBListJSON(new File(fileJson));
+        arg = ArgumentManager.checkArgumentsList(args);
+
 
         /*
         Scanner s = new Scanner(System.in);
 
         System.out.println("Inserisci il percorso di destinazione dei file generati da RING");
         String path = s.next();
-        */
+*/
         for(String singlePDB : pdbList) {
             TsvReader.reader(new String[]{singlePDB,fileJson});
-            Ring.ringManager(new String[]{singlePDB,args[0]});
+            Ring.ringManager(new String[]{singlePDB,arg[0]});
             //MainAasGenerator.main(new String[]{singlePDB,}); TODO qui va messo lo start come secondo paramentro
         }
 

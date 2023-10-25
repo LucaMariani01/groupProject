e8f56d7a8c50b715f1c014e2a567aaff3cbd19e6
luca <marianiluca01@gmail.com>
2023-10-25 11:17:26 +0200

    fix duplicate aas

--- aasGeneretor/src/main/java/AasParser.java
+++ aasGeneretor/src/main/java/AasParser.java
@@ -6,31 +6,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class AasParser {
-    /*public ArrayList<String> readerEdges(File fileTSV,int start){
-        ArrayList<String>  result = new ArrayList<>();
-        String lista_amminoacidi = "",lista_legami="";
-        int cont =0;
-        try (BufferedReader TSVReader = new BufferedReader(new FileReader(fileTSV))) {
-            String line;
-            while ((line = TSVReader.readLine()) != null) {
-                if(cont== 0) cont=1;
-                else {
-                    String[] lineItems = line.split(":");
-                    lista_amminoacidi= lista_amminoacidi+this.parser(lineItems[3].substring(0,3))
-                            + this.parser(lineItems[7].substring(0,3));
-                    lista_legami = lista_legami+ "("+((Integer.parseInt(lineItems[1])-start)+1)+","
-                            +((Integer.parseInt(lineItems[5])-start)+1)+");";
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("Something went wrong AAS errore :"+e);
-        }
-
-        result.add(lista_amminoacidi);
-        result.add(lista_legami);
-        return result;
-    }*/
-
     /**
      * This method is used to write the aas file after parsing the pdb edges file
      * @param pdbEdgesFile is the file containing the pdb's list of edges
@@ -39,7 +14,7 @@ public class AasParser {
      */
     public ArrayList<String> readerEdges(File pdbEdgesFile,int start){
         ArrayList<String>  result = new ArrayList<>();
-        String aminoAcidList = "",bondList="";
+        String aminoAcidList = "",bondList="",lastAminoAcidList="" , lastBond="";
         int cont =0;
         try (BufferedReader pdbEdgesReader = new BufferedReader(new FileReader(pdbEdgesFile))) {
             String line;
@@ -47,10 +22,19 @@ public class AasParser {
                 if(cont== 0) cont=1;
                 else {
                     String[] lineItems = line.split(":");
-                    aminoAcidList= aminoAcidList+this.parser(lineItems[3].substring(0,3))
-                            + this.parser(lineItems[7].substring(0,3));
-                    bondList = bondList+"("+((Integer.parseInt(lineItems[1])-start)+1)+","
-                            +((Integer.parseInt(lineItems[5])-start)+1)+");";
+                    if (lastAminoAcidList.compareTo(this.parser(lineItems[3].substring(0, 3))
+                            + this.parser(lineItems[7].substring(0, 3)))!=0 && lastBond.compareTo("(" + ((Integer.parseInt(lineItems[1]) - start) + 1) + ","
+                            + ((Integer.parseInt(lineItems[5]) - start) + 1) + ");")!=0) {
+
+                        aminoAcidList = aminoAcidList + this.parser(lineItems[3].substring(0, 3))
+                                + this.parser(lineItems[7].substring(0, 3));
+                        bondList = bondList + "(" + ((Integer.parseInt(lineItems[1]) - start) + 1) + ","
+                                + ((Integer.parseInt(lineItems[5]) - start) + 1) + ");";
+                        lastAminoAcidList=this.parser(lineItems[3].substring(0, 3))
+                                + this.parser(lineItems[7].substring(0, 3));
+                        lastBond= "(" + ((Integer.parseInt(lineItems[1]) - start) + 1) + ","
+                                + ((Integer.parseInt(lineItems[5]) - start) + 1) + ");";
+                    }
                 }
             }
         } catch (Exception e) { System.out.println("Something went wrong during aas generation: "+e); }

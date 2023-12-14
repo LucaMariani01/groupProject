2727f3dbf23dd46682adc644277f7292d2cd4161
Diezz01 <diegopennesi01@gmail.com>
2023-12-14 18:15:09 +0100

    fix create exec time file

--- mainModule/src/main/java/TimeController.java
+++ mainModule/src/main/java/TimeController.java
@@ -14,7 +14,7 @@ public class TimeController {
     public static void executionTimeManager(long jsonTime, long ringTime, long aasTime, String fileName){
         try {
             FileWriter writer = new FileWriter(fileName, true);
-            writer.write(jsonTime + "\t" + ringTime + "\t" + aasTime + "\n");
+            writer.write(jsonTime + ";" + ringTime + ";" + aasTime + ";");
             writer.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
